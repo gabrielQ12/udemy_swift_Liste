327a59d83a682d8d9556189114e16db0b2dad5ed
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-11 16:52:47 +0100

    mise en plqce : DONE

---
+++ LesGardiensDeLaGalaxies/LesGardiensDeLaGalaxies/Guardian.swift
@@ -0,0 +1,16 @@
+//
+//  Guardian.swift
+//  LesGardiensDeLaGalaxies
+//
+//  Created by Gaby on 11/02/2024.
+//
+
+import Foundation
+
+struct Gardian {
+var name: String
+var desc: String
+var imagePath: String
+}
+
+
